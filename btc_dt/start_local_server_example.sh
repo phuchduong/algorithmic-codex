44158d988850c40e3b15c7e62bc860591c0c7d43
Phuc H Duong <phuchduong@hotmail.com>
2026-04-11 21:29:44 -0400

    BTC data table.

---
+++ btc_dt/start_local_server_example.sh
@@ -0,0 +1,5 @@
+PS C:\repos\algorithmic-codex> cd btc_dt
+PS C:\repos\algorithmic-codex\btc_dt> py -m http.server 8000
+# Serving HTTP on :: port 8000 (http://[::]:8000/) ...
+
+# GO TO CHROME: http://localhost:8000/index.html
